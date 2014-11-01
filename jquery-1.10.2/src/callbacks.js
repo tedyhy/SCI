1829bf28fb77b298cf8d5ba29b15a2b6011d283e
tedYHY <yaohiyang@gmail.com>
2014-11-02 00:31:55 +0800

    callback

--- jquery-1.10.2/src/callbacks.js
+++ jquery-1.10.2/src/callbacks.js
@@ -1,4 +1,10 @@
-// 管理回调模块
+/*
+ * jQuery.Callbacks是jquery在1.7版本之后加入的，是从1.6版中的_Deferred对象中抽离的，
+ * 主要用来进行对函数队列list的 add、remove、fire、lock、disable 等操作，
+ * 并提供 once、memory、unique、stopOnFalse 四个option进行一些特殊的控制，
+ * jquery的官方文档：http://api.jquery.com/jQuery.Callbacks/
+ * 博客讲解：http://www.cnblogs.com/lmule/p/3463515.html
+ */
 // String to Object options format cache
 // Callbacks选项缓存，如：jQuery.Callbacks("once memory")
 // { "once memory": {"once": true, "memory": true} }
@@ -23,18 +29,53 @@ function createOptions( options ) {
  * By default a callback list will act like an event callback list and can be
  * "fired" multiple times.
  *
+ * 两个例子方法：
+ *	function fn1( value ) {
+ *	  console.log( value );
+ *	}
+ *	 
+ *	function fn2( value ) {
+ *	  console.log( "fn2 says: " + value );
+ *	  return false;
+ *	}
+ *
  * Possible options:
  *
  *	once:			will ensure the callback list can only be fired once (like a Deferred)
  *					确保回调列表中函数仅仅被触发一次。
+ *					（即：仅仅list中的第一个回调被触发，list中的后续的回调都不会再被触发了）。
+ *					看例子：
+ *					var callbacks = $.Callbacks( "once" );
+ *					callbacks.add( fn1 );
+ *					callbacks.fire( "foo" );
+ *					callbacks.add( fn2 );
+ *					callbacks.fire( "bar" );
+ *					callbacks.remove( fn2 );
+ *					callbacks.fire( "foobar" );
+ *					output: foo
+ *
  *	memory:			will keep track of previous values and will call any callback added
  *					after the list has been fired right away with the latest "memorized"
  *					values (like a Deferred)
+ *					记忆参数。看下面的例子：
+ *					var callbacks = $.Callbacks( "memory" );
+ *					callbacks.add( fn1 );
+ *					callbacks.fire( "foo" );
+ *					callbacks.add( fn2 );
+ *					callbacks.fire( "bar" );
+ *					callbacks.remove( fn2 );
+ *					callbacks.fire( "foobar" );
+ *					output:
+ *						foo
+ *						fn2 says:foo
+ *						bar
+ *						fn2 says:bar
+ *						foobar
  *
  *	unique:			will ensure a callback can only be added once (no duplicate in the list)
  *					确保每个回调唯一，不允许有重复的回调存在。
  *	stopOnFalse:	interrupt callings when a callback returns false
- *					当一个回调返回false，则中断所有回调的触发。
+ *					当一个回调返回false，则中断剩余所有回调的触发。
  */
 jQuery.Callbacks = function( options ) {
 
@@ -48,9 +89,10 @@ jQuery.Callbacks = function( options ) {
 		// 标记list中的回调是否正在被触发
 		firing,
 		// Last fire value (for non-forgettable lists)
+		// 最后触发的值???
 		memory,
 		// Flag to know if list was already fired
-		// 标记list里的回调是否被触发，哪怕至少一个被触发。
+		// 标记list是否已经被触发过。
 		fired,
 		// End of the loop when firing
 		// 记录正在被触发的回调列表长度。
@@ -59,19 +101,20 @@ jQuery.Callbacks = function( options ) {
 		// 当前正在被触发的回调的索引（移除回调的时候会修改这个参数）。
 		firingIndex,
 		// First callback to fire (used internally by add and fireWith)
+		// 第一个被触发的回调索引。
 		firingStart,
 		// Actual callback list
 		// 回调列表（用来存储回调）。
 		list = [],
 		// Stack of fire calls for repeatable lists
-		// 堆栈，存储可能要重复触发的回调。
+		// 一个堆栈，用于临时存储回调函数（那些要在list触发完成之后触发的函数）。
 		// 如果options.once===true时，stack为undefined。
 		// 如果options.once===false时，stack初始化为空数组。
 		stack = !options.once && [],
 		// Fire callbacks
-		// 触发回调
+		// 触发回调list，参数data=[context, args]
 		fire = function( data ) {
-			memory = options.memory && data;
+			memory = options.memory && data; //???
 			fired = true;
 			firingIndex = firingStart || 0;
 			firingStart = 0;
@@ -87,9 +130,9 @@ jQuery.Callbacks = function( options ) {
 			if ( list ) {
 				if ( stack ) {
 					if ( stack.length ) {
-						fire( stack.shift() );
+						fire( stack.shift() ); // 自调用
 					}
-				} else if ( memory ) {
+				} else if ( memory ) { //???
 					list = [];
 				} else {
 					self.disable();
