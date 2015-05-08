663b94ce0207cc87551008f2e8eb0b5a58bb86a3
tedyhy <916610351@qq.com>
2015-05-08 15:46:21 +0800

    init

--- artDialog-6.0.4/src/dialog-config.js
+++ artDialog-6.0.4/src/dialog-config.js
@@ -5,10 +5,10 @@ define({
 
     // 对齐方式
     //align: 'bottom left',
-    
+
     // 是否固定定位
     //fixed: false,
-    
+
     // 对话框叠加高度值(重要：此值不能超过浏览器最大限制)
     //zIndex: 1024,
 
@@ -20,39 +20,39 @@ define({
 
     // 消息内容
     content: '<span class="ui-dialog-loading">Loading..</span>',
-    
+
     // 标题
     title: '',
 
     // 对话框状态栏区域 HTML 代码
     statusbar: '',
-    
+
     // 自定义按钮
     button: null,
-    
+
     // 确定按钮回调函数
     ok: null,
-    
+
     // 取消按钮回调函数
     cancel: null,
 
     // 确定按钮文本
     okValue: 'ok',
-    
+
     // 取消按钮文本
     cancelValue: 'cancel',
 
     cancelDisplay: true,
-    
+
     // 内容宽度
     width: '',
-    
+
     // 内容高度
     height: '',
-    
+
     // 内容与边界填充距离
     padding: '',
-    
+
     // 对话框自定义 className
     skin: '',
 
